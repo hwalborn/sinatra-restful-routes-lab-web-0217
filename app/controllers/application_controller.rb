2dbdb4817709455eff4861293f881b8aa3f59917
hwalborn <hwalborn@gmail.com>
2017-02-21 17:42:01 -0500

    Cookbook

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,38 @@ class ApplicationController < Sinatra::Base
   get '/' do
     erb :index
   end
+
+  get '/recipes/new' do
+    erb :new
+  end
+
+  post '/recipes' do
+    @recipe = Recipe.create(params)
+    redirect to "/recipes/#{@recipe.id}"
+  end
+
+  get '/recipes' do
+    @recipes = Recipe.all
+    erb :recipes
+  end
+
+  get '/recipes/:id' do
+    @recipe = Recipe.find(params[:id])
+    erb :show
+  end
+
+  get '/recipes/:id/edit' do
+    @recipe = Recipe.find(params[:id])
+    erb :edit
+  end
+
+  delete '/recipes/:id/delete' do
+    Recipe.delete(params[:id])
+  end
+
+  patch '/recipes/:id' do
+    Recpie.update(params[:id], name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time])
+  end
+
+
 end
---
+++ db/migrate/001_create_table_incredients.rb
@@ -0,0 +1,7 @@
+class CreateTableIncredients < ActiveRecord::Migration
+  create_table :recipes do |t|
+    t.string :name
+    t.string :ingredients
+    t.string :cook_time
+  end
+end
